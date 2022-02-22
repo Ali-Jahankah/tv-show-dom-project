a71caaa604e1f41cb6e20fa31f6d8b76a6a629b4
Ali Jahankah <aral2@yahoo.com>
2022-02-22 21:43:29 +0000

    creating header and main

--- script.js
+++ script.js
@@ -1,12 +1,70 @@
 //You can edit ALL of the code here
+const rootTag = document.getElementById("root");
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  const Episodes = getAllEpisodes();
+  //------------------Header-------------------
+  const header = document.createElement("header");
+  header.className = "header";
+  //nav
+  const nav = document.createElement("nav");
+  nav.className = "nav";
+  //nav menu
+  const nav_menu_div = document.createElement("div");
+  nav_menu_div.className = "nav_menu_div";
+  //nav search
+  const nav_search_div = document.createElement("div");
+  nav_search_div.className = "nav_search_div";
+  const nav_search = document.createElement("input");
+  nav_search.className = "nav_search";
+  nav_search_div.appendChild(nav_search);
+  nav_search.value = "";
+  nav_search.placeholder = "search...";
+
+  nav_search.addEventListener("change", (e) => {
+    handleSearch(e);
+  })
+  //nav select
+  const nav_select_div = document.createElement("div");
+  nav_select_div.className = "nav_select_div";
+  const nav_select = document.createElement("select");
+  nav_select_div.appendChild(nav_select);
+  nav_select.value = "none";
+  nav_select.className = "nav_select";
+  //Creating header by appending the children
+  header.appendChild(nav);
+  nav.append(nav_menu_div, nav_search_div, nav_select_div);
+  //--------------------------Main
+  //Nav SVG
+  const wave = document.createElement("img");
+  wave.src = "./svg/wave.svg";
+  wave.className = "nav_svg";
+  header.appendChild(wave);
+
+
+
+  const episodesArticle = document.createElement("article");
+  rootTag.append(header, episodesArticle);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+
 
 window.onload = setup;
